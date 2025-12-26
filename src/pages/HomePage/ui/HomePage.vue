a38fe21fb61eedf5471f692e5c8aee590a2abe4f
krel <molchanov_i_v@mail.ru>
2025-12-26 23:52:41 +0500

    add Header and NetrumState widget to HomePage

--- src/pages/HomePage/ui/HomePage.vue
+++ src/pages/HomePage/ui/HomePage.vue
@@ -1,5 +1,25 @@
-<script lang="ts" setup></script>
+<template>
+  <Header></Header>
+  <NetrumStat></NetrumStat>
+</template>
+
+<script setup>
+import Header from '~/widigets/Header'
+import NetrumStat from '~/widigets/NetrumStat'
+</script>
+
+<!-- <script lang="ts" setup>
+const link = '/api/nodes/stats'
+// const link = '/data/test.json'
+const { data } = await useFetch(link, {
+  method: 'get',
+  server: false,
+  onResponse({ response }) {
+    console.log(response)
+  },
+})
+</script>
 
 <template>Главная</template>
 
-<style scoped lang="sass"></style>
+<style scoped lang="sass"></style> -->
